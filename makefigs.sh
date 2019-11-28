4f46ac6406918b860841d5b35883a851ac21f283
Trygve Leithe Svalheim <svalheim@obelisk.uio.no>
2019-11-28 13:56:11 +0100

    added makefigs

---
+++ makefigs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python spectrum.py
+python spectrum.py darkmode
+python spectrum.py long
+python spectrum.py long darkmode
+python spectrum.py pol
+python spectrum.py darkmode pol
+python spectrum.py long pol
+python spectrum.py long darkmode pol
